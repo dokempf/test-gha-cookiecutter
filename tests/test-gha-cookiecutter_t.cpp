79bdc0c9754ba4a442f7c5e74a7ef24d690e6f32
SSC CI Test User <ssc@citestuser.com>
2026-01-26 05:38:21 +0000

    Initial Commit

---
+++ tests/test-gha-cookiecutter_t.cpp
@@ -0,0 +1,11 @@
+#include "test-gha-cookiecutter/test-gha-cookiecutter.hpp"
+#include <catch2/catch_test_macros.hpp>
+
+using namespace testghacookiecutter;
+
+TEST_CASE("add_one", "[adder]")
+{
+  REQUIRE(add_one(0) == 1);
+  REQUIRE(add_one(123) == 124);
+  REQUIRE(add_one(-1) == 0);
+}
